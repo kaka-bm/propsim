90dc545039020f405aff81e844bde94d67908606
gabrielurbanin <gabrielut97@hotmail.com>
2022-11-19 15:08:13 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='propsim',
+    version='0.0.1',
+    description='A simple and intuitive tool for simulating different types of aircraft engines.',
+    py_modules=["propsim"],
+    package_dir={'':'src'}
+)
