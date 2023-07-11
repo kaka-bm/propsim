85486ea9826b729b4cdb13d263ded19f48f7f89e
Carlos Muniz <66801632+kaka-bm@users.noreply.github.com>
2023-07-11 12:12:28 -0300

    Update propsim.py

--- src/propsim.py
+++ src/propsim.py
@@ -1042,9 +1042,9 @@ class EnginesNozzles:
 
 
 
-        # Eq 3.11 para condição de M0
-
-        ## A0_At = ((A / Astar) * (Pty / P0)) não precisa, fiz uma função
+        # Eq 3.11 para condição de M0 (não foi possível obter a equação diretamente sendo necessário uma função)
+        
+        ## A0_At = ((A / Astar) * (Pty / P0))  
 
         # Função feita a partir do gráfico 10.23 do livro Elements of Propulsion
         def fun_A0_At(M):
